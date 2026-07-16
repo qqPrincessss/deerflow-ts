96925c4d385dc185050e3a238230401b326d4006
qingqingPrincess <574498398@qq.com>
2026-07-16 16:31:59 +0800

    chore: clean up - keep only layer 0 files

--- backend/packages/harness/src/config/index.ts
+++
@@ -1,2 +0,0 @@
-export { getAppConfig, getModelConfig, getDefaultModelConfig, AppConfigSchema, type AppConfig } from "./app-config.js";
-export { ModelConfigSchema, resolveConfigValue, type ModelConfig } from "./model-config.js";
